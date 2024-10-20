e6d2526d42cdd492011b8c240ecf5633d7d47645
Vitor Justamand <justamandd@gmail.com>
2024-10-20 17:38:39 -0300

    feat: routes added to index file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 // src/index.ts
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { routes } from "@http/routes";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(routes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
